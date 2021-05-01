4f26b8022c36daaa55b55744f6f5a37fa562d03b
purofle <3272912942@qq.com>
2021-05-01 21:10:24 +0800

    .

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from application import KaiHeiLaApplication
 from application.event.kaiheila import GroupMessage
 from graia.broadcast import Broadcast
 from config import token
-from application.group import Member
+from application.group import Group, Member
 
 loop = asyncio.get_event_loop()
 
@@ -13,7 +13,7 @@ app = KaiHeiLaApplication(token=token, broadcast=bcc)
 
 
 @bcc.receiver(GroupMessage)
-async def tme(kha: KaiHeiLaApplication, member: Member, message: str):
+async def tme(kha: KaiHeiLaApplication, member: Member, group: Group, message: str):
     print("收到消息：{}".format(message))
     print(member.id)
 
